388e82aa4af51878f2e1c6d24ab0652cd82fa02d
huongnv <huongnv@atomi.com.vn>
2023-09-07 10:21:24 +0700

    docker compose version 2

---
+++ docker-compose/17.nextcloud/nextcloud/data/config/config.php
@@ -0,0 +1,32 @@
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+  'instanceid' => 'ocddbt4a5i95',
+  'passwordsalt' => '7clywuKeR9igoKOhXtPn1VM72BY+U5',
+  'secret' => 'J6r4oj1LyWQaBvLIqESWmP3phvO3AQKQPD3PTM2oscDaUkLW',
+  'trusted_domains' => 
+  array (
+    0 => 'localhost:81',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'sqlite3',
+  'version' => '27.0.2.1',
+  'overwrite.cli.url' => 'http://localhost:81',
+  'installed' => true,
+);
